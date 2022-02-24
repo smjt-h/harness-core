37a2a8ee0456ff7ce8f2c6e24a3d89ee25a85beb
Pragyesh Mishra <pragyesh.mishra@harness.io>
2022-02-25 03:17:15 +0530

    [CDS-33749]: Added function for get serverless infra config

--- 127-cd-nextgen-beans/src/main/java/io/harness/cdng/serverless/ServerlessEntityHelper.java
+++ 127-cd-nextgen-beans/src/main/java/io/harness/cdng/serverless/ServerlessEntityHelper.java
@@ -8,29 +8,61 @@
 package io.harness.cdng.serverless;
 
 import static io.harness.connector.ConnectorModule.DEFAULT_CONNECTOR_SERVICE;
+import static io.harness.data.structure.EmptyPredicate.isNotEmpty;
+import static io.harness.delegate.task.serverless.ServerlessInfraType.AWS_INFRA;
 import static io.harness.exception.WingsException.USER;
 
 import static java.lang.String.format;
+import static java.util.Collections.emptyList;
 
 import io.harness.annotations.dev.HarnessTeam;
 import io.harness.annotations.dev.OwnedBy;
+import io.harness.beans.DecryptableEntity;
 import io.harness.beans.IdentifierRef;
+import io.harness.cdng.infra.beans.InfrastructureOutcome;
+import io.harness.cdng.infra.beans.ServerlessAwsInfrastructureOutcome;
+import io.harness.cdng.infra.yaml.InfrastructureKind;
 import io.harness.connector.ConnectorInfoDTO;
 import io.harness.connector.ConnectorResponseDTO;
 import io.harness.connector.services.ConnectorService;
+import io.harness.delegate.beans.connector.awsconnector.AwsConnectorDTO;
+import io.harness.delegate.task.serverless.ServerlessAwsInfraConfig;
+import io.harness.delegate.task.serverless.ServerlessInfraConfig;
 import io.harness.exception.InvalidRequestException;
 import io.harness.ng.core.NGAccess;
+import io.harness.secretmanagerclient.services.api.SecretManagerClientService;
+import io.harness.security.encryption.EncryptedDataDetail;
 import io.harness.utils.IdentifierRefHelper;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.google.inject.name.Named;
+import java.util.List;
 import java.util.Optional;
+import javax.annotation.Nonnull;
 
 @OwnedBy(HarnessTeam.CDP)
 @Singleton
 public class ServerlessEntityHelper {
+  @Named("PRIVILEGED") @Inject private SecretManagerClientService secretManagerClientService;
   @Named(DEFAULT_CONNECTOR_SERVICE) @Inject private ConnectorService connectorService;
+
+  public List<EncryptedDataDetail> getEncryptionDataDetails(
+      @Nonnull ConnectorInfoDTO connectorDTO, @Nonnull NGAccess ngAccess) {
+    switch (connectorDTO.getConnectorType()) {
+      case AWS:
+        AwsConnectorDTO awsConnectorDTO = (AwsConnectorDTO) connectorDTO.getConnectorConfig();
+        List<DecryptableEntity> awsDecryptableEntities = awsConnectorDTO.getDecryptableEntities();
+        if (isNotEmpty(awsDecryptableEntities)) {
+          return secretManagerClientService.getEncryptionDetails(ngAccess, awsDecryptableEntities.get(0));
+        } else {
+          return emptyList();
+        }
+      default:
+        throw new UnsupportedOperationException(
+            format("Unsupported connector type : [%s]", connectorDTO.getConnectorType()));
+    }
+  }
   public ConnectorInfoDTO getConnectorInfoDTO(String connectorId, NGAccess ngAccess) {
     IdentifierRef identifierRef = IdentifierRefHelper.getIdentifierRef(
         connectorId, ngAccess.getAccountIdentifier(), ngAccess.getOrgIdentifier(), ngAccess.getProjectIdentifier());
@@ -40,5 +72,25 @@ public class ServerlessEntityHelper {
       throw new InvalidRequestException(format("Connector not found for identifier : [%s] ", connectorId), USER);
     }
     return connectorDTO.get().getConnector();
+  }
+
+  public ServerlessInfraConfig getServerlessInfraConfig(
+      InfrastructureOutcome infrastructureOutcome, NGAccess ngAccess) {
+    ConnectorInfoDTO connectorDTO = getConnectorInfoDTO(infrastructureOutcome.getConnectorRef(), ngAccess);
+    switch (infrastructureOutcome.getKind()) {
+      case InfrastructureKind.SERVERLESS_AWS:
+        ServerlessAwsInfrastructureOutcome serverlessAwsInfrastructureOutcome =
+            (ServerlessAwsInfrastructureOutcome) infrastructureOutcome;
+        return ServerlessAwsInfraConfig.builder()
+            .encryptionDataDetails(getEncryptionDataDetails(connectorDTO, ngAccess))
+            .awsConnectorDTO((AwsConnectorDTO) connectorDTO.getConnectorConfig())
+            .serverlessInfraType(AWS_INFRA)
+            .region(serverlessAwsInfrastructureOutcome.getRegion())
+            .stage(serverlessAwsInfrastructureOutcome.getStage())
+            .build();
+      default:
+        throw new UnsupportedOperationException(
+            format("Unsupported Infrastructure type: [%s]", infrastructureOutcome.getKind()));
+    }
   }
 }
