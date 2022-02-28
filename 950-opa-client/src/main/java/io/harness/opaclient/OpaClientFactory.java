08011888a2c96d16857d7d14168db3062c13b3ae
Ankit Singhal <ankit.singhal@harness.io>
2022-02-28 14:35:26 +0530

    Update OpaClientFactory.java

--- 950-opa-client/src/main/java/io/harness/opaclient/OpaClientFactory.java
+++ 950-opa-client/src/main/java/io/harness/opaclient/OpaClientFactory.java
@@ -22,7 +22,6 @@ import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
-import io.serializer.HObjectMapper;
 import java.io.IOException;
 import java.io.InputStream;
 import java.security.KeyStore;
