15469a754bb3725864e450f517af8b84c97a5e04
Jenny James <jenny.james@harness.io>
2022-03-05 09:48:44 -0800

    test

--- 400-rest/src/main/java/software/wings/service/impl/DelegateServiceImpl.java
+++ 400-rest/src/main/java/software/wings/service/impl/DelegateServiceImpl.java
@@ -2475,8 +2475,9 @@ public class DelegateServiceImpl implements DelegateService {
     if (existingDelegate == null) {
       log.info("No existing delegate, adding for account {}: Hostname: {} IP: {}", delegate.getAccountId(),
           delegate.getHostName(), delegate.getIp());
-
-      createAuditHeaderForDelegateRegistration(delegate.getHostName());
+      if (!delegate.isNg()){
+        createAuditHeaderForDelegateRegistration(delegate.getHostName());
+      }
 
       registeredDelegate = add(delegate);
       sendTelemetryTrackEvents(
