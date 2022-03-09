11237839dc16f6e98b4c1b0b9ac42e6c906afa35
Vladimir Peric <vladimir.peric@harness.io>
2022-03-09 17:16:29 +0100

    [feat]:[CDS-34378]: Test connection API (#31316)

--- 950-ng-core/src/main/java/io/harness/ng/validator/HostValidationService.java
+++ 950-ng-core/src/main/java/io/harness/ng/validator/HostValidationService.java
@@ -3,10 +3,11 @@ package io.harness.ng.validator;
 import io.harness.connector.HostValidationResult;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface HostValidationService {
   List<HostValidationResult> validateSSHHosts(List<String> hosts, String accountIdentifier, String orgIdentifier,
-      String projectIdentifier, String secretIdentifier);
+      String projectIdentifier, String secretIdentifier, Optional<Integer> limit);
 
   HostValidationResult validateSSHHost(
       String host, String accountIdentifier, String orgIdentifier, String projectIdentifier, String secretIdentifier);
