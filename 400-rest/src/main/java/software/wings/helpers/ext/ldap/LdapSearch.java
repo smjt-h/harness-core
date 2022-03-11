fc866860b6e914e4f65e0ff392d7ac8acca4cf19
Ujjawal <ujjawal.prasad@harness.io>
2022-03-11 10:04:42 +0530

    feat: [PL-23661]: Changes in pagination query

--- 400-rest/src/main/java/software/wings/helpers/ext/ldap/LdapSearch.java
+++ 400-rest/src/main/java/software/wings/helpers/ext/ldap/LdapSearch.java
@@ -169,6 +169,18 @@ public class LdapSearch implements LdapValidator {
           ctx.setRequestControls(new Control[] {new PagedResultsControl(pageSize, cookie, Control.CRITICAL)});
         } while (cookie != null);
         ctx.close();
+        if (entries.isEmpty()) {
+          try (Connection connection = connectionFactory.getConnection()) {
+            log.info(
+                "Trying to query to LDAP server with old logic as entries not found with pagination with searchfilter {} and baseDN {}",
+                searchFilter, baseDN);
+            connection.open();
+            SearchOperation search = new SearchOperation(connection);
+            return search.execute(request).getResult();
+          } catch (Exception e) {
+            log.error("Error querying to LDAP server with old logic as entries not found with pagination  ", e);
+          }
+        }
         return new SearchResult(entries);
       } catch (Exception e) {
         log.error("Error querying to ldap server with pagination", e);
