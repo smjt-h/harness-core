4e22b2e933ce243a4097073d32ebcbe382ea686f
Tejas Tiwari <tejas.tiwari@harness.io>
2022-03-14 11:54:30 +0530

    [PL-22385]: Added SM and secret cretaion service for blob

---
+++ 890-sm-core/src/main/java/io/harness/mappers/AzureBlobConfigMapper.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2022 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Free Trial 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
+ */
+
+package io.harness.mappers;
+
+import static io.harness.annotations.dev.HarnessTeam.PL;
+import static io.harness.eraro.ErrorCode.SECRET_MANAGEMENT_ERROR;
+import static io.harness.exception.WingsException.USER;
+import static io.harness.mappers.SecretManagerConfigMapper.ngMetaDataFromDto;
+
+import io.harness.annotations.dev.OwnedBy;
+import io.harness.exception.SecretManagementException;
+import io.harness.ng.core.mapper.TagMapper;
+import io.harness.secretmanagerclient.NGSecretManagerMetadata;
+import io.harness.secretmanagerclient.dto.azureblob.AzureBlobConfigDTO;
+import io.harness.secretmanagerclient.dto.azureblob.AzureBlobConfigUpdateDTO;
+
+import software.wings.beans.AzureBlobConfig;
+
+import java.util.Optional;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+@OwnedBy(PL)
+public class AzureBlobConfigMapper {
+  public static AzureBlobConfig fromDTO(AzureBlobConfigDTO azureBlobConfigDTO) {
+    AzureBlobConfig azureBlobConfig = AzureBlobConfig.builder()
+                                          .name(azureBlobConfigDTO.getName())
+                                          .clientId(azureBlobConfigDTO.getClientId())
+                                          .secretKey(azureBlobConfigDTO.getSecretKey())
+                                          .subscription(azureBlobConfigDTO.getSubscription())
+                                          .tenantId(azureBlobConfigDTO.getTenantId())
+                                          .azureEnvironmentType(azureBlobConfigDTO.getAzureEnvironmentType())
+                                          .vaultName(azureBlobConfigDTO.getVaultName())
+                                          .delegateSelectors(azureBlobConfigDTO.getDelegateSelectors())
+                                          .build();
+    azureBlobConfig.setNgMetadata(ngMetaDataFromDto(azureBlobConfigDTO));
+    azureBlobConfig.setAccountId(azureBlobConfigDTO.getAccountIdentifier());
+    azureBlobConfig.setEncryptionType(azureBlobConfigDTO.getEncryptionType());
+    azureBlobConfig.setDefault(azureBlobConfigDTO.isDefault());
+    return azureBlobConfig;
+  }
+
+  private static void checkEqualValues(Object x, Object y, String fieldName) {
+    if (x != null && !x.equals(y)) {
+      throw new SecretManagementException(SECRET_MANAGEMENT_ERROR,
+          String.format(
+              "Cannot change the value of %s since there are secrets already present in azure key blob. Please delete or migrate them and try again.",
+              fieldName),
+          USER);
+    }
+  }
+
+  public static AzureBlobConfig applyUpdate(
+      AzureBlobConfig blobConfig, AzureBlobConfigUpdateDTO updateDTO, boolean secretsPresentInBlob) {
+    if (secretsPresentInBlob) {
+      checkEqualValues(blobConfig.getVaultName(), updateDTO.getVaultName(), "vault name");
+    }
+    blobConfig.setClientId(updateDTO.getClientId());
+    blobConfig.setSubscription(updateDTO.getSubscription());
+    blobConfig.setAzureEnvironmentType(updateDTO.getAzureEnvironmentType());
+    blobConfig.setTenantId(updateDTO.getTenantId());
+    blobConfig.setVaultName(updateDTO.getVaultName());
+    if (Optional.ofNullable(updateDTO.getSecretKey()).isPresent()) {
+      blobConfig.setSecretKey(updateDTO.getSecretKey());
+    }
+    blobConfig.setDefault(updateDTO.isDefault());
+    blobConfig.setName(updateDTO.getName());
+
+    if (!Optional.ofNullable(blobConfig.getNgMetadata()).isPresent()) {
+      blobConfig.setNgMetadata(NGSecretManagerMetadata.builder().build());
+    }
+    blobConfig.getNgMetadata().setTags(TagMapper.convertToList(updateDTO.getTags()));
+    blobConfig.getNgMetadata().setDescription(updateDTO.getDescription());
+    return blobConfig;
+  }
+}
---
+++ 890-sm-core/src/main/java/software/wings/beans/AzureBlobConfig.java
@@ -0,0 +1,173 @@
+/*
+ * Copyright 2022 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Free Trial 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
+ */
+
+package software.wings.beans;
+
+import static io.harness.annotations.dev.HarnessTeam.PL;
+import static io.harness.azure.AzureEnvironmentType.AZURE;
+import static io.harness.beans.SecretManagerCapabilities.*;
+import static io.harness.beans.SecretManagerCapabilities.TRANSITION_SECRET_TO_SM;
+import static io.harness.data.structure.EmptyPredicate.isNotEmpty;
+import static io.harness.expression.SecretString.SECRET_MASK;
+import static io.harness.security.encryption.SecretManagerType.VAULT;
+
+import io.harness.annotations.dev.OwnedBy;
+import io.harness.azure.AzureEnvironmentType;
+import io.harness.beans.SecretManagerCapabilities;
+import io.harness.beans.SecretManagerConfig;
+import io.harness.delegate.beans.executioncapability.ExecutionCapability;
+import io.harness.delegate.beans.executioncapability.SelectorCapability;
+import io.harness.delegate.task.mixin.HttpConnectionExecutionCapabilityGenerator;
+import io.harness.encryption.Encrypted;
+import io.harness.expression.ExpressionEvaluator;
+import io.harness.mappers.SecretManagerConfigMapper;
+import io.harness.secretmanagerclient.dto.SecretManagerConfigDTO;
+import io.harness.secretmanagerclient.dto.azureblob.AzureBlobConfigDTO;
+import io.harness.security.encryption.EncryptionType;
+import io.harness.security.encryption.SecretManagerType;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.github.reinert.jjschema.Attributes;
+import com.github.reinert.jjschema.SchemaIgnore;
+import com.google.common.collect.Lists;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import lombok.*;
+import lombok.experimental.FieldNameConstants;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.validator.constraints.NotEmpty;
+
+@OwnedBy(PL)
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(exclude = {"secretKey"})
+@EqualsAndHashCode(callSuper = true)
+@FieldNameConstants(innerTypeName = "AzureBlobConfigKeys")
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class AzureBlobConfig extends SecretManagerConfig {
+  private static final String TASK_SELECTORS = "Task Selectors";
+  public static final String AZURE_VAULT_VALIDATION_URL = "harnessAzureVaultValidation";
+  @Attributes(title = "Name", required = true) private String name;
+
+  @Attributes(title = "Azure Client Id", required = true) @NotEmpty private String clientId;
+
+  @Attributes(title = "Azure Secret Id", required = true)
+  @NotEmpty
+  @Encrypted(fieldName = "azure_secret_id")
+  private String secretKey;
+
+  @Attributes(title = "Azure Tenant Id", required = true) @NotEmpty private String tenantId;
+
+  @Attributes(title = "Azure Vault Name", required = true) private String vaultName;
+
+  @Attributes(title = "Subscription") private String subscription;
+
+  @Attributes(title = "Azure Storage Connection String", required = true) @NotEmpty private String connectionString;
+
+  @Attributes(title = "Azure Storage Container Name", required = true) @NotEmpty private String containerName;
+
+  @Attributes(title = "Azure Key Vault Key ID", required = true) @NotEmpty private String keyId;
+
+  @Attributes(title = "delegateSelectors") private Set<String> delegateSelectors;
+
+  @Builder.Default private AzureEnvironmentType azureEnvironmentType = AZURE;
+
+  @Override
+  public void maskSecrets() {
+    this.secretKey = SECRET_MASK;
+  }
+
+  @JsonIgnore
+  @SchemaIgnore
+  @Override
+  public String getValidationCriteria() {
+    return obtainEncryptionServiceUrl();
+  }
+
+  @Override
+  public SecretManagerType getType() {
+    return VAULT;
+  }
+
+  @JsonIgnore
+  @SchemaIgnore
+  @Override
+  public String getEncryptionServiceUrl() {
+    return obtainEncryptionServiceUrl();
+  }
+
+  @Override
+  public List<ExecutionCapability> fetchRequiredExecutionCapabilities(ExpressionEvaluator maskingEvaluator) {
+    List<ExecutionCapability> executionCapabilities =
+        new ArrayList<>(Arrays.asList(HttpConnectionExecutionCapabilityGenerator.buildHttpConnectionExecutionCapability(
+            getEncryptionServiceUrl(), maskingEvaluator)));
+    if (isNotEmpty(getDelegateSelectors())) {
+      executionCapabilities.add(
+          SelectorCapability.builder().selectors(getDelegateSelectors()).selectorOrigin(TASK_SELECTORS).build());
+    }
+    return executionCapabilities;
+  }
+
+  @Override
+  public EncryptionType getEncryptionType() {
+    return EncryptionType.AZURE_VAULT;
+  }
+
+  private String obtainEncryptionServiceUrl() {
+    if (this.azureEnvironmentType == null) {
+      return String.format("https://%s.vault.azure.net", getVaultName());
+    }
+
+    switch (this.azureEnvironmentType) {
+      case AZURE_US_GOVERNMENT:
+        return String.format("https://%s.vault.usgovcloudapi.net", getVaultName());
+
+      case AZURE:
+      default:
+        return String.format("https://%s.vault.azure.net", getVaultName());
+    }
+  }
+
+  @Override
+  public List<SecretManagerCapabilities> getSecretManagerCapabilities() {
+    List<SecretManagerCapabilities> secretManagerCapabilities =
+        Lists.newArrayList(CREATE_INLINE_SECRET, CREATE_REFERENCE_SECRET, CREATE_FILE_SECRET, CAN_BE_DEFAULT_SM);
+    if (!isTemplatized()) {
+      secretManagerCapabilities.add(TRANSITION_SECRET_FROM_SM);
+      secretManagerCapabilities.add(TRANSITION_SECRET_TO_SM);
+    }
+    return secretManagerCapabilities;
+  }
+
+  @Override
+  public SecretManagerConfigDTO toDTO(boolean maskSecrets) {
+    AzureBlobConfigDTO ngAzureBlobConfigDTO = AzureBlobConfigDTO.builder()
+                                                  .encryptionType(getEncryptionType())
+                                                  .name(getName())
+                                                  .isDefault(isDefault())
+                                                  .clientId(getClientId())
+                                                  .tenantId(getTenantId())
+                                                  .vaultName(getVaultName())
+                                                  .subscription(getSubscription())
+                                                  .connectionString(getConnectionString())
+                                                  .containerName(getContainerName())
+                                                  .keyId(getKeyId())
+                                                  .azureEnvironmentType(getAzureEnvironmentType())
+                                                  .delegateSelectors(getDelegateSelectors())
+                                                  .build();
+    SecretManagerConfigMapper.updateNGSecretManagerMetadata(getNgMetadata(), ngAzureBlobConfigDTO);
+    if (!maskSecrets) {
+      ngAzureBlobConfigDTO.setSecretKey(getSecretKey());
+    }
+    return ngAzureBlobConfigDTO;
+  }
+}
