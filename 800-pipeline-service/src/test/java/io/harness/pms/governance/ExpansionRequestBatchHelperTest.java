e9760d6b5fd2a16c33fe4b8165a55e31f7f212e0
Naman Verma <naman.verma@harness.io>
2022-03-15 14:34:24 +0530

    add test

--- 800-pipeline-service/src/test/java/io/harness/pms/governance/ExpansionRequestBatchHelperTest.java
+++ 800-pipeline-service/src/test/java/io/harness/pms/governance/ExpansionRequestBatchHelperTest.java
@@ -6,21 +6,80 @@ import static io.harness.rule.OwnerRule.NAMAN;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import io.harness.CategoryTest;
+import io.harness.ModuleType;
 import io.harness.annotations.dev.OwnedBy;
 import io.harness.category.element.UnitTests;
+import io.harness.pms.contracts.governance.ExpansionRequestBatch;
+import io.harness.pms.contracts.governance.ExpansionRequestMetadata;
+import io.harness.pms.contracts.governance.ExpansionRequestProto;
 import io.harness.pms.yaml.YamlField;
 import io.harness.pms.yaml.YamlNode;
 import io.harness.pms.yaml.YamlUtils;
 import io.harness.rule.Owner;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.TextNode;
 import com.google.protobuf.ByteString;
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
 @OwnedBy(PIPELINE)
 public class ExpansionRequestBatchHelperTest extends CategoryTest {
+  @Test
+  @Owner(developers = NAMAN)
+  @Category(UnitTests.class)
+  public void testGetBatchesByModule() {
+    ExpansionRequest cdK8sConn1 = ExpansionRequest.builder()
+                                      .fqn("fqn1")
+                                      .module(ModuleType.CD)
+                                      .key("connectorRef")
+                                      .fieldValue(new TextNode("k8sConn"))
+                                      .build();
+    ExpansionRequest cdK8sConn2 = ExpansionRequest.builder()
+                                      .fqn("fqn2")
+                                      .module(ModuleType.CD)
+                                      .key("connectorRef")
+                                      .fieldValue(new TextNode("k8sConn"))
+                                      .build();
+    ExpansionRequest cdDockerConn = ExpansionRequest.builder()
+                                        .fqn("fqn3")
+                                        .module(ModuleType.CD)
+                                        .key("connectorRef")
+                                        .fieldValue(new TextNode("dockerConn"))
+                                        .build();
+    ExpansionRequest ciK8sConn1 = ExpansionRequest.builder()
+                                      .fqn("fqn4")
+                                      .module(ModuleType.CI)
+                                      .key("connectorRef")
+                                      .fieldValue(new TextNode("k8sConn"))
+                                      .build();
+    ExpansionRequest ciK8sConn2 = ExpansionRequest.builder()
+                                      .fqn("fqn5")
+                                      .module(ModuleType.CI)
+                                      .key("connectorRef")
+                                      .fieldValue(new TextNode("k8sConn"))
+                                      .build();
+    Set<ExpansionRequest> expansionRequests =
+        Stream.of(cdK8sConn1, cdK8sConn2, cdDockerConn, ciK8sConn1, ciK8sConn2).collect(Collectors.toSet());
+    ExpansionRequestMetadata metadata =
+        ExpansionRequestMetadata.newBuilder().setAccountId("acc").setOrgId("org").setProjectId("proj").build();
+    ExpansionRequestData batchesByModule = ExpansionRequestBatchHelper.getBatchesByModule(expansionRequests, metadata);
+    Map<ModuleType, ExpansionRequestBatch> moduleToRequestBatch = batchesByModule.getModuleToRequestBatch();
+    assertThat(moduleToRequestBatch).hasSize(2);
+    List<ExpansionRequestProto> cdRequests = moduleToRequestBatch.get(ModuleType.CD).getExpansionRequestProtoList();
+    assertThat(cdRequests).hasSize(2);
+    List<ExpansionRequestProto> ciRequests = moduleToRequestBatch.get(ModuleType.CI).getExpansionRequestProtoList();
+    assertThat(ciRequests).hasSize(1);
+    Map<String, Set<String>> uuidToFqnSet = batchesByModule.getUuidToFqnSet();
+    assertThat(uuidToFqnSet).hasSize(3);
+  }
+
   @Test
   @Owner(developers = NAMAN)
   @Category(UnitTests.class)
