0bb8f596e574a6579aeb100818c7a7ecd2fed312
Mankrit Singh <mankrit.singh@harness.io>
2022-03-16 22:19:02 +0530

    [PL-22547]: Removing 948 Access Control SDK

--- scripts/bazel/bazel_script.sh
+++ scripts/bazel/bazel_script.sh
@@ -133,7 +133,6 @@ BAZEL_MODULES="\
   //945-license-usage-sdk:module \
   //945-ng-audit-client:module \
   //947-scim-core:module \
-  //948-access-control-sdk:module \
   //950-command-library-common:module \
   //959-common-entities:module \
   //950-delegate-tasks-beans/src/main/proto:all \
