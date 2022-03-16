991b614308ddebfac5d288311ace418bdce929df
Mankrit Singh <mankrit.singh@harness.io>
2022-03-16 21:56:24 +0530

    [PL-22547]: Removing Access Control 80-aggregator

--- scripts/bazel/bazel_script.sh
+++ scripts/bazel/bazel_script.sh
@@ -193,7 +193,6 @@ BAZEL_MODULES="\
   //990-commons-test:module \
   //999-annotations:module \
   //access-control/service:module \
-  //access-control/libraries/80-aggregator:module \
   //access-control/libraries/90-core:module \
   //access-control/contracts:module \
   //product/ci/engine/proto:all \
