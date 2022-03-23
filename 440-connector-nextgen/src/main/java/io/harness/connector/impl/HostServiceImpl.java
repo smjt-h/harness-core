ccb2eae9a353a2dafdb4f9aec6157c6530dc0a25
Bojan Micovic <bojan.micovic@harness.io>
2022-03-23 11:56:16 +0100

    feat: [CDS-34172]: Fixing paging for the API for getting hosts

--- 440-connector-nextgen/src/main/java/io/harness/connector/impl/HostServiceImpl.java
+++ 440-connector-nextgen/src/main/java/io/harness/connector/impl/HostServiceImpl.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2022 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Free Trial 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
+ */
+
 package io.harness.connector.impl;
 
 import static io.harness.data.structure.EmptyPredicate.isEmpty;
@@ -56,8 +63,8 @@ public class HostServiceImpl implements HostService {
     List<HostDTO> hosts = applyFilter(connectorDTO.getHosts(), filter);
 
     List<HostDTO> pageResponse = hosts.stream()
-                                     .limit(pageRequest.getPageSize())
                                      .skip((long) pageRequest.getPageIndex() * pageRequest.getPageSize())
+                                     .limit(pageRequest.getPageSize())
                                      .sorted(Comparator.comparing(HostDTO::getHostName))
                                      .collect(toList());
     return new PageImpl<>(pageResponse, getPageRequest(pageRequest), hosts.size());
