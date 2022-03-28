67133c672b1348db24e4346dd8cb7eeaa88e4396
Naman Verma <naman.verma@harness.io>
2022-03-28 12:14:19 +0530

    --code format

--- 800-pipeline-service/src/main/java/io/harness/pms/notification/NotificationHelper.java
+++ 800-pipeline-service/src/main/java/io/harness/pms/notification/NotificationHelper.java
@@ -154,9 +154,8 @@ public class NotificationHelper {
 
   List<NotificationRules> getNotificationRulesFromYaml(String yaml, Ambiance ambiance) throws IOException {
     BasicPipeline basicPipeline = YamlUtils.read(yaml, BasicPipeline.class);
-    return RecastOrchestrationUtils.fromMap(
-        (Map<String, Object>) pmsEngineExpressionService.resolve(
-            ambiance, RecastOrchestrationUtils.toMap(basicPipeline.getNotificationRules())),
+    return RecastOrchestrationUtils.fromMap((Map<String, Object>) pmsEngineExpressionService.resolve(ambiance,
+                                                RecastOrchestrationUtils.toMap(basicPipeline.getNotificationRules())),
         List.class);
   }
 
