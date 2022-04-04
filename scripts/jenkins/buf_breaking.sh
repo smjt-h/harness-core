662e1ab79e73d5a25ddf4adbe0d67cbf316ede9f
kanikamittal09 <kanika.mittal@harness.io>
2022-04-04 15:35:42 +0530

    buf breaking script with package mode

---
+++ scripts/jenkins/buf_breaking.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+git fetch origin $1
+git checkout $1
+EXIT_CODE=0
+output=""
+IFS=$'\n' bufOut=($(buf breaking --against '.git#branch=develop'))
+for m in $(git log develop..$1 --format=format:%H )
+do
+    echo $m
+    for k in $(git show --pretty="" --name-only $m | grep \.proto$)
+    do         
+        echo $k
+        
+        for l in "${bufOut[@]}"
+        do
+            if grep -q "$k" <<< "$l";
+            then
+                output+=$l
+                output+="\n"
+                echo $l
+                EXIT_CODE=100
+            fi
+        done
+    done  
+done   
+echo $EXIT_CODE >> exit_code.txt
+echo $output >> output.txt   
