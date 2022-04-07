a6aad47dab8497df5fb24297e2be3d6b42c53af7
Brijesh Dhakar <brijesh.dhakar@harness.io>
2022-04-07 11:25:25 +0530

    [fix]: [PIE-3408]: Sending module=PIPELINE in pipeline audit events instead of Core

--- 970-ng-commons/src/main/java/io/harness/ModuleType.java
+++ 970-ng-commons/src/main/java/io/harness/ModuleType.java
@@ -25,6 +25,7 @@ public enum ModuleType {
   // Internal
   @JsonProperty("CORE") CORE("Core", true),
   @JsonProperty("PMS") PMS("Pipelines", true),
+  @JsonProperty("PIPELINE") PIPELINE("Pipeline", true),
   @JsonProperty("TEMPLATESERVICE") TEMPLATESERVICE("TemplateService", true);
 
   String displayName;
