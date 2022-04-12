8e47fe46c00d997deaab2730b24e78442be2480c
Prashant Pal <prashant.pal@harness.io>
2022-04-12 11:39:00 +0530

    feat: [PL-24541]: Formatting changes

--- 900-git-sync-sdk/src/main/java/io/harness/gitsync/v2/GitAware.java
+++ 900-git-sync-sdk/src/main/java/io/harness/gitsync/v2/GitAware.java
@@ -8,14 +8,12 @@ import io.harness.persistence.PersistentEntity;
  * The SDK should never deal with the entity itself, all the interactions will be driven by the interface
  */
 public interface GitAware extends PersistentEntity, WithIdentifier {
-
   // Defines where this entity is stored
   StoreType getStoreType();
 
   // TODO : As this will be a blob does it make sense for this to be a byte[]
   // The String representation of the resource mostly yaml string
   String getData();
-
 
   // Repo in which the entity will be saved
   String getRepo();
