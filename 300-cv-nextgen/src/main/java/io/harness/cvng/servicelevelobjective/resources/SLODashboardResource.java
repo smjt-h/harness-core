476988f40027510be6fbfed5a113958753a9c266
Dhruv <dhruv@harness.io>
2022-04-14 12:49:32 +0530

    [feat]: [SRM-10013]: Refactoring resource layer to use ProjectParam via @BeanParam. (#32203)
    * [SRM-10013]: Reformatting AppDynamicsResource.java to use ProjectParam object.
    
    * [SRM-10013]: Reformatting ServiceLevelObjectiveResource.java, SLODashboardResource.java and UserJourneyResource.java to use ProjectParam object.
    
    * [SRM-10013]: Reformatting core, dashboard and verification resources to use ProjectParam object.

--- 300-cv-nextgen/src/main/java/io/harness/cvng/servicelevelobjective/resources/SLODashboardResource.java
+++ 300-cv-nextgen/src/main/java/io/harness/cvng/servicelevelobjective/resources/SLODashboardResource.java
@@ -34,7 +34,6 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 
 @Api("slo-dashboard")
 @Path("slo-dashboard")
