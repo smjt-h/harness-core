a0fdf5a0c0bf8e0447e998a4b811f225238e76cd
Raghvendra Singh <raghu@harness.io>
2022-04-18 11:41:24 -0700

    [fix]: [DEL-3804]: fix unit tests

--- 930-delegate-tasks/src/main/java/io/harness/serializer/morphia/DelegateTasksMorphiaRegistrar.java
+++ 930-delegate-tasks/src/main/java/io/harness/serializer/morphia/DelegateTasksMorphiaRegistrar.java
@@ -7,18 +7,20 @@
 
 package io.harness.serializer.morphia;
 
+import io.harness.beans.SecretManagerConfig;
 import io.harness.morphia.MorphiaRegistrar;
 import io.harness.morphia.MorphiaRegistrarHelperPut;
 
-import software.wings.beans.AppDynamicsConfig;
-import software.wings.beans.DynaTraceConfig;
-import software.wings.beans.EcrConfig;
-import software.wings.beans.ElkConfig;
-import software.wings.beans.NewRelicConfig;
-import software.wings.beans.SumoConfig;
+import software.wings.beans.*;
+import software.wings.beans.appmanifest.HelmChart;
+import software.wings.beans.artifact.ArtifactFile;
+import software.wings.beans.config.ArtifactoryConfig;
 import software.wings.beans.config.LogzConfig;
+import software.wings.beans.config.NexusConfig;
+import software.wings.beans.settings.azureartifacts.AzureArtifactsPATConfig;
 import software.wings.delegatetasks.cv.beans.CustomLogResponseMapper;
 import software.wings.delegatetasks.validation.DelegateConnectionResult;
+import software.wings.helpers.ext.mail.SmtpConfig;
 import software.wings.service.impl.analysis.CustomLogDataCollectionInfo;
 import software.wings.service.impl.analysis.DataCollectionTaskResult;
 import software.wings.service.impl.elk.ElkDataCollectionInfo;
@@ -33,6 +35,10 @@ public class DelegateTasksMorphiaRegistrar implements MorphiaRegistrar {
   public void registerClasses(Set<Class> set) {
     set.add(NewRelicMetricDataRecord.class);
     set.add(DelegateConnectionResult.class);
+    set.add(HelmChart.class);
+    set.add(BaseVaultConfig.class);
+    set.add(SecretManagerConfig.class);
+    set.add(SSHVaultConfig.class);
   }
 
   @Override
@@ -50,5 +56,25 @@ public class DelegateTasksMorphiaRegistrar implements MorphiaRegistrar {
     w.put("service.impl.elk.ElkDataCollectionInfo", ElkDataCollectionInfo.class);
     w.put("service.impl.logz.LogzDataCollectionInfo", LogzDataCollectionInfo.class);
     w.put("beans.EcrConfig", EcrConfig.class);
+    w.put("beans.SecretManagerConfig", SecretManagerConfig.class);
+    w.put("beans.SSHVaultConfig", SSHVaultConfig.class);
+    w.put("beans.BaseVaultConfig", BaseVaultConfig.class);
+    w.put("beans.GcpConfig", GcpConfig.class);
+    w.put("beans.artifact.ArtifactFile", ArtifactFile.class);
+    w.put("beans.ServiceNowConfig", ServiceNowConfig.class);
+    w.put("beans.DockerConfig", DockerConfig.class);
+    w.put("beans.config.NexusConfig", NexusConfig.class);
+    w.put("beans.JiraConfig", JiraConfig.class);
+    w.put("beans.SplunkConfig", SplunkConfig.class);
+    w.put("beans.settings.azureartifacts.AzureArtifactsPATConfig", AzureArtifactsPATConfig.class);
+    w.put("beans.SftpConfig", SftpConfig.class);
+    w.put("beans.InstanaConfig", InstanaConfig.class);
+    w.put("beans.HostConnectionAttributes", HostConnectionAttributes.class);
+    w.put("beans.PcfConfig", PcfConfig.class);
+    w.put("beans.AzureConfig", AzureConfig.class);
+    w.put("beans.BambooConfig", BambooConfig.class);
+    w.put("beans.SmbConfig", SmbConfig.class);
+    w.put("beans.config.ArtifactoryConfig", ArtifactoryConfig.class);
+    w.put("helpers.ext.mail.SmtpConfig", SmtpConfig.class);
   }
 }
