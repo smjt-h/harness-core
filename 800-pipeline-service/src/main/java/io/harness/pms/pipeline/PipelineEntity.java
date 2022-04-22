54e7412a87f9b5a615d8fcb6e400f0e0f676978b
Naman Verma <naman.verma@harness.io>
2022-04-22 12:09:09 +0530

    unused import

--- 800-pipeline-service/src/main/java/io/harness/pms/pipeline/PipelineEntity.java
+++ 800-pipeline-service/src/main/java/io/harness/pms/pipeline/PipelineEntity.java
@@ -15,7 +15,6 @@ import io.harness.annotations.ChangeDataCapture;
 import io.harness.annotations.dev.OwnedBy;
 import io.harness.data.validator.EntityName;
 import io.harness.data.validator.Trimmed;
-import io.harness.gitsync.persistance.GitSyncableEntity;
 import io.harness.gitsync.v2.GitAware;
 import io.harness.gitsync.v2.StoreType;
 import io.harness.mongo.index.CompoundMongoIndex;
