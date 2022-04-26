2561bad951ea3e25076acf07fce53d6ccfd35bfa
Lucas Sales <lucas.sales@harness.io>
2022-04-26 10:42:57 -0300

    adding test case

--- 400-rest/src/test/java/software/wings/helpers/ext/jenkins/JenkinsTest.java
+++ 400-rest/src/test/java/software/wings/helpers/ext/jenkins/JenkinsTest.java
@@ -535,6 +535,25 @@ public class JenkinsTest extends WingsBaseTest {
   @Test
   @Owner(developers = DEEPAK_PUTHRAYA)
   @Category(UnitTests.class)
+  public void shouldGetAllJobsFromJenkinsWithOrganizationFolder() throws IOException {
+    wireMockRule.stubFor(
+        get(urlEqualTo("/api/json"))
+            .willReturn(
+                aResponse()
+                    .withStatus(200)
+                    .withBody(
+                        "{\"_class\":\"com.cloudbees.hudson.plugins.folder.Folder\",\"property\":[{},{\"_class\":\"hudson.plugins.jobConfigHistory.JobConfigHistoryProjectAction\"},{},{\"_class\":\"com.cloudbees.plugins.credentials.ViewCredentialsAction\"}],\"description\":null,\"displayName\":\"parentJob\",\"displayNameOrNull\":null,\"fullDisplayName\":\"parentJob\",\"fullName\":\"parentJob\",\"name\":\"parentJob\",\"url\":\"https://jenkins.wings.software/job/\",\"healthReport\":[],\"jobs\":[{\"_class\":\"jenkins.branch.OrganizationFolder\",\"name\":\"abcd\",\"url\":\"https://jenkins.wings.software/job/parentJob/job/abcd/\"},{\"_class\":\"hudson.maven.MavenModuleSet\",\"name\":\"parentJob_war_copy\",\"url\":\"https://jenkins.wings.software/job/parentJob/job/parentJob_war_copy/\",\"color\":\"notbuilt\"}],\"primaryView\":{\"_class\":\"hudson.model.AllView\",\"name\":\"All\",\"url\":\"https://jenkins.wings.software/job/parentJob/\"},\"views\":[{\"_class\":\"hudson.model.AllView\",\"name\":\"All\",\"url\":\"https://jenkins.wings.software/job/parentJob/\"}]}")
+                    .withHeader("Content-Type", "application/json")));
+
+    List<JobDetails> jobs = jenkins.getJobs("");
+    assertThat(jobs.get(0).isFolder()).isTrue();
+    assertThat(jobs.get(1).isFolder()).isFalse();
+    assertThat(jobs.size() == 2).isTrue();
+  }
+
+  @Test
+  @Owner(developers = DEEPAK_PUTHRAYA)
+  @Category(UnitTests.class)
   public void triggerThrowErrorJobNotFound() throws IOException {
     CustomJenkinsServer jenkinsServer = mock(CustomJenkinsServer.class);
     Reflect.on(jenkins).set("jenkinsServer", jenkinsServer);
