2104da9e33304c6f3e3edd5707a4b13cdcdbaec1
Yogesh Chauhan <yogesh.chauhan@harness.io>
2022-05-04 23:39:11 +0530

    feat: [CDS-36903]: buildifier
    Signed-off-by: Yogesh Chauhan <yogesh.chauhan@harness.io>

--- 127-cd-nextgen-entities/src/main/java/io/harness/serializer/morphia/NGEntitiesMorphiaRegistrar.java
+++ 127-cd-nextgen-entities/src/main/java/io/harness/serializer/morphia/NGEntitiesMorphiaRegistrar.java
@@ -6,13 +6,12 @@ import io.harness.ng.core.service.entity.ServiceEntity;
 
 import java.util.Set;
 
-public class NGEntitiesMorphiaRegistrar implements MorphiaRegistrar  {
-    @Override
-    public void registerClasses(Set<Class> set) {
-        set.add(ServiceEntity.class);
-    }
+public class NGEntitiesMorphiaRegistrar implements MorphiaRegistrar {
+  @Override
+  public void registerClasses(Set<Class> set) {
+    set.add(ServiceEntity.class);
+  }
 
-    @Override
-    public void registerImplementationClasses(MorphiaRegistrarHelperPut h, MorphiaRegistrarHelperPut w) {
-    }
+  @Override
+  public void registerImplementationClasses(MorphiaRegistrarHelperPut h, MorphiaRegistrarHelperPut w) {}
 }
