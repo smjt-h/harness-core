6f66531a1c4ca82e5c922292720665fe8c9372cd
Yogesh Chauhan <yogesh.chauhan@harness.io>
2022-05-05 13:38:23 +0530

    feat: [CDS-36609]: broken develop fix
    Signed-off-by: Yogesh Chauhan <yogesh.chauhan@harness.io>

--- 127-cd-nextgen-entities/src/main/java/io/harness/serializer/morphia/NGEntitiesMorphiaRegistrar.java
+++ 127-cd-nextgen-entities/src/main/java/io/harness/serializer/morphia/NGEntitiesMorphiaRegistrar.java
@@ -3,6 +3,7 @@ package io.harness.serializer.morphia;
 import io.harness.cdng.artifact.outcome.AcrArtifactOutcome;
 import io.harness.cdng.artifact.outcome.DockerArtifactOutcome;
 import io.harness.cdng.artifact.outcome.GcrArtifactOutcome;
+import io.harness.cdng.service.beans.ServiceConfigOutcome;
 import io.harness.cdng.service.beans.ServiceOutcome;
 import io.harness.morphia.MorphiaRegistrar;
 import io.harness.morphia.MorphiaRegistrarHelperPut;
@@ -23,5 +24,6 @@ public class NGEntitiesMorphiaRegistrar implements MorphiaRegistrar {
     h.put("ngpipeline.artifact.bean.DockerArtifactOutcome", DockerArtifactOutcome.class);
     h.put("ngpipeline.artifact.bean.GcrArtifactOutcome", GcrArtifactOutcome.class);
     h.put("ngpipeline.artifact.bean.AcrArtifactOutcome", AcrArtifactOutcome.class);
+    h.put("cdng.service.beans.ServiceConfigOutcome", ServiceConfigOutcome.class);
   }
 }
