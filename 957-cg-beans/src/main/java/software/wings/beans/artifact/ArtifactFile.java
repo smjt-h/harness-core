d85927a50415885df94ce7fdf50634e60bc5c203
Abhinav Mittal <abhinav.mittal@harness.io>
2022-05-05 22:06:05 +0530

    [CDS-36210]: update codehash in delegtate module and remove target module from a few classes

--- 957-cg-beans/src/main/java/software/wings/beans/artifact/ArtifactFile.java
+++ 957-cg-beans/src/main/java/software/wings/beans/artifact/ArtifactFile.java
@@ -9,9 +9,7 @@ package software.wings.beans.artifact;
 
 import static io.harness.annotations.dev.HarnessTeam.CDC;
 
-import io.harness.annotations.dev.HarnessModule;
 import io.harness.annotations.dev.OwnedBy;
-import io.harness.annotations.dev.TargetModule;
 import io.harness.beans.EmbeddedUser;
 import io.harness.delegate.beans.ChecksumType;
 
@@ -21,7 +19,6 @@ import software.wings.beans.BaseFile;
  * The Class ArtifactFile.
  */
 @OwnedBy(CDC)
-@TargetModule(HarnessModule._957_CG_BEANS)
 public class ArtifactFile extends BaseFile {
   /**
    * The type Builder.
