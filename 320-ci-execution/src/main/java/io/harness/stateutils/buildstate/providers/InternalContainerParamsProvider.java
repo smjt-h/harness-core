ef575d2a26c03f2640d17633244ffead6c5f4a4d
Shubham Agrawal <shubham.agrawal@harness.io>
2022-05-05 16:39:58 +0530

    [CI-3965]: CI manager changes for windows builds in kubernetes

--- 320-ci-execution/src/main/java/io/harness/stateutils/buildstate/providers/InternalContainerParamsProvider.java
+++ 320-ci-execution/src/main/java/io/harness/stateutils/buildstate/providers/InternalContainerParamsProvider.java
@@ -23,9 +23,10 @@ import static io.harness.common.CIExecutionConstants.HARNESS_WORKSPACE;
 import static io.harness.common.CIExecutionConstants.LITE_ENGINE_CONTAINER_CPU;
 import static io.harness.common.CIExecutionConstants.LITE_ENGINE_CONTAINER_MEM;
 import static io.harness.common.CIExecutionConstants.LITE_ENGINE_CONTAINER_NAME;
-import static io.harness.common.CIExecutionConstants.SETUP_ADDON_ARGS;
+import static io.harness.common.CIExecutionConstants.PWSH_COMMAND;
 import static io.harness.common.CIExecutionConstants.SETUP_ADDON_CONTAINER_NAME;
 import static io.harness.common.CIExecutionConstants.SH_COMMAND;
+import static io.harness.common.CIExecutionConstants.UNIX_SETUP_ADDON_ARGS;
 import static io.harness.data.encoding.EncodingUtils.encodeBase64;
 import static io.harness.delegate.beans.ci.pod.SecretParams.Type.TEXT;
 
@@ -33,6 +34,7 @@ import io.harness.annotations.dev.HarnessTeam;
 import io.harness.annotations.dev.OwnedBy;
 import io.harness.beans.FeatureName;
 import io.harness.beans.sweepingoutputs.K8PodDetails;
+import io.harness.beans.yaml.extended.infrastrucutre.OSType;
 import io.harness.ci.config.CIExecutionServiceConfig;
 import io.harness.ci.integrationstage.IntegrationStageUtils;
 import io.harness.delegate.beans.ci.pod.CIContainerType;
@@ -68,14 +70,18 @@ public class InternalContainerParamsProvider {
 
   public CIK8ContainerParams getSetupAddonContainerParams(ConnectorDetails harnessInternalImageConnector,
       Map<String, String> volumeToMountPath, String workDir, ContainerSecurityContext ctrSecurityContext,
-      String accountIdentifier) {
-    List<String> args = Arrays.asList(SETUP_ADDON_ARGS);
+      String accountIdentifier, OSType os) {
+    List<String> args = Arrays.asList(UNIX_SETUP_ADDON_ARGS);
     Map<String, String> envVars = new HashMap<>();
     envVars.put(HARNESS_WORKSPACE, workDir);
 
     String imageName = ciExecutionConfigService.getAddonImage(accountIdentifier);
     String fullyQualifiedImage =
         IntegrationStageUtils.getFullyQualifiedImageName(imageName, harnessInternalImageConnector);
+    List<String> commands = SH_COMMAND;
+    if (os.equals(OSType.WINDOWS)) {
+      commands = PWSH_COMMAND;
+    }
     return CIK8ContainerParams.builder()
         .name(SETUP_ADDON_CONTAINER_NAME)
         .envVars(envVars)
@@ -85,7 +91,7 @@ public class InternalContainerParamsProvider {
                                        .build())
         .containerSecrets(ContainerSecrets.builder().build())
         .volumeToMountPath(volumeToMountPath)
-        .commands(SH_COMMAND)
+        .commands(commands)
         .args(args)
         .securityContext(ctrSecurityContext)
         .containerResourceParams(getAddonResourceParams())
