97cb859d606272929ccf70346376734f2988526a
Ved Tambat <ved.atultambat@harness.io>
2022-05-05 19:01:34 +0530

    [CDS-36970]: ChaosHubResourceImpl

---
+++ platform-service/modules/resource-group-service/src/main/java/io/harness/resourcegroup/resourceclient/chaoshub/ChaosHubResourceImpl.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2022 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Free Trial 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
+ */
+
+package io.harness.resourcegroup.resourceclient.chaoshub;
+
+import static io.harness.resourcegroup.beans.ValidatorType.BY_RESOURCE_IDENTIFIER;
+import static io.harness.resourcegroup.beans.ValidatorType.BY_RESOURCE_TYPE;
+import static io.harness.resourcegroup.beans.ValidatorType.BY_RESOURCE_TYPE_INCLUDING_CHILD_SCOPES;
+
+import io.harness.beans.Scope;
+import io.harness.beans.ScopeLevel;
+import io.harness.eventsframework.EventsFrameworkMetadataConstants;
+import io.harness.eventsframework.consumer.Message;
+import io.harness.resourcegroup.beans.ValidatorType;
+import io.harness.resourcegroup.framework.v1.service.Resource;
+import io.harness.resourcegroup.framework.v1.service.ResourceInfo;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.inject.Inject;
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@AllArgsConstructor(access = AccessLevel.PUBLIC, onConstructor = @__({ @Inject }))
+@Slf4j
+public class ChaosHubResourceImpl implements Resource {
+  @Override
+  public String getType() {
+    return "CHAOS_HUB";
+  }
+
+  @Override
+  public Set<ScopeLevel> getValidScopeLevels() {
+    return EnumSet.of(ScopeLevel.ACCOUNT, ScopeLevel.ORGANIZATION, ScopeLevel.PROJECT);
+  }
+
+  @Override
+  public Optional<String> getEventFrameworkEntityType() {
+    return Optional.of(EventsFrameworkMetadataConstants.CHAOS_HUB);
+  }
+
+  @Override
+  public ResourceInfo getResourceInfoFromEvent(Message message) {
+    return null;
+  }
+
+  @Override
+  public List<Boolean> validate(List<String> resourceIds, Scope scope) {
+    return null;
+  }
+
+  @Override
+  public Map<ScopeLevel, EnumSet<ValidatorType>> getSelectorKind() {
+    return return ImmutableMap.of(ScopeLevel.ACCOUNT, EnumSet.of(BY_RESOURCE_TYPE_INCLUDING_CHILD_SCOPES),
+        ScopeLevel.ORGANIZATION, EnumSet.of(BY_RESOURCE_TYPE_INCLUDING_CHILD_SCOPES), ScopeLevel.PROJECT,
+        EnumSet.of(BY_RESOURCE_IDENTIFIER, BY_RESOURCE_TYPE));
+  }
+}
