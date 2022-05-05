0702b7e31fa859ead6b6d5bc3ad49000ae136c95
Ved Tambat <ved.atultambat@harness.io>
2022-05-05 19:30:26 +0530

    [CDS-36970]: getResourceInfoFromEvent

--- platform-service/modules/resource-group-service/src/main/java/io/harness/resourcegroup/resourceclient/chaoshub/ChaosHubResourceImpl.java
+++ platform-service/modules/resource-group-service/src/main/java/io/harness/resourcegroup/resourceclient/chaoshub/ChaosHubResourceImpl.java
@@ -11,21 +11,21 @@ import static io.harness.resourcegroup.beans.ValidatorType.BY_RESOURCE_IDENTIFIE
 import static io.harness.resourcegroup.beans.ValidatorType.BY_RESOURCE_TYPE;
 import static io.harness.resourcegroup.beans.ValidatorType.BY_RESOURCE_TYPE_INCLUDING_CHILD_SCOPES;
 
+import static org.apache.commons.lang3.StringUtils.stripToNull;
+
 import io.harness.beans.Scope;
 import io.harness.beans.ScopeLevel;
 import io.harness.eventsframework.EventsFrameworkMetadataConstants;
 import io.harness.eventsframework.consumer.Message;
+import io.harness.eventsframework.entity_crud.EntityChangeDTO;
 import io.harness.resourcegroup.beans.ValidatorType;
 import io.harness.resourcegroup.framework.v1.service.Resource;
 import io.harness.resourcegroup.framework.v1.service.ResourceInfo;
 
 import com.google.common.collect.ImmutableMap;
 import com.google.inject.Inject;
-import java.util.EnumSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
+import com.google.protobuf.InvalidProtocolBufferException;
+import java.util.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -52,7 +52,22 @@ public class ChaosHubResourceImpl implements Resource {
 
   @Override
   public ResourceInfo getResourceInfoFromEvent(Message message) {
-    return null;
+    EntityChangeDTO entityChangeDTO = null;
+    try {
+      entityChangeDTO = EntityChangeDTO.parseFrom(message.getMessage().getData());
+    } catch (InvalidProtocolBufferException e) {
+      log.error("Exception in unpacking EntityChangeDTO for key {}", message.getId(), e);
+    }
+    if (Objects.isNull(entityChangeDTO)) {
+      return null;
+    }
+    return ResourceInfo.builder()
+        .accountIdentifier(stripToNull(entityChangeDTO.getAccountIdentifier().getValue()))
+        .orgIdentifier(stripToNull(entityChangeDTO.getOrgIdentifier().getValue()))
+        .projectIdentifier(stripToNull(entityChangeDTO.getProjectIdentifier().getValue()))
+        .resourceType(getType())
+        .resourceIdentifier(entityChangeDTO.getIdentifier().getValue())
+        .build();
   }
 
   @Override
@@ -62,7 +77,7 @@ public class ChaosHubResourceImpl implements Resource {
 
   @Override
   public Map<ScopeLevel, EnumSet<ValidatorType>> getSelectorKind() {
-    return return ImmutableMap.of(ScopeLevel.ACCOUNT, EnumSet.of(BY_RESOURCE_TYPE_INCLUDING_CHILD_SCOPES),
+    return ImmutableMap.of(ScopeLevel.ACCOUNT, EnumSet.of(BY_RESOURCE_TYPE_INCLUDING_CHILD_SCOPES),
         ScopeLevel.ORGANIZATION, EnumSet.of(BY_RESOURCE_TYPE_INCLUDING_CHILD_SCOPES), ScopeLevel.PROJECT,
         EnumSet.of(BY_RESOURCE_IDENTIFIER, BY_RESOURCE_TYPE));
   }
