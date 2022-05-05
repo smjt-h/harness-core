69aeb9050e3277e2f255e3ac707eb81ca486e026
Sahil Hindwani <sahil.hindwani@harness.io>
2022-05-05 17:42:51 +0530

    [PIE-3619]: Better error handling if delegate task could not be queued from pipeline-service

---
+++ 980-commons/src/main/java/io/harness/exception/QueueTaskException.java
@@ -0,0 +1,15 @@
+package io.harness.exception;
+
+import static io.harness.eraro.ErrorCode.QUEUE_TASK_EXCEPTION;
+
+import io.harness.eraro.Level;
+import io.harness.exception.ngexception.ErrorMetadataDTO;
+
+public class QueueTaskException extends WingsException {
+  private static final String MESSAGE_ARG = "message";
+
+  public QueueTaskException(String message) {
+    super(message, null, QUEUE_TASK_EXCEPTION, Level.ERROR, null, null);
+    super.param(MESSAGE_ARG, message);
+  }
+}
